e28a286d3138be13e1425e2ad4b9e5fb4fc821d0
Mary Rose Cook <mary@maryrosecook.com>
2017-08-27 17:45:13 +0100

    Don't track out directory

--- out/cljs_deps.js
+++
@@ -1,4 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.math.Integer', 'goog.string.StringBuffer', 'goog.array', 'goog.math.Long']);
-goog.addDependency("../animation/core.js", ['animation.core'], ['goog.dom', 'cljs.core', 'goog.events']);
-goog.addDependency("../process/env.js", ['process.env'], ['cljs.core']);
