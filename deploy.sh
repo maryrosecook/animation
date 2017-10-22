42a06a43471713a8b47e0133764266153fb8661f
Mary Rose Cook <mary@maryrosecook.com>
2017-10-22 16:46:01 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+rsync -au index.html do:/var/www/animation.maryrosecook.com/
+rsync -au resources/ do:/var/www/animation.maryrosecook.com/resources/
+rsync -au out/ do:/var/www/animation.maryrosecook.com/out/
